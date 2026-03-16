99dbc61ccad432f05be5f0e0e99ea2a860c6c0ba
anaiah <carlodominguez@yahoo.com>
2026-03-16 22:55:02 +0800

    v1

--- assets/js/ipconfig.js
+++ assets/js/ipconfig.js
@@ -4,4 +4,4 @@
 //const myIp  = "https://esndp-gis-jku4q.ondigitalocean.app"
 //const myIp  = "https://asianowapp-jtx-lqum6.ondigitalocean.app"
 //const myIp= "https://asn-jtgrp-api.onrender.com"
-const myIp  = "https://leslie-gis.onrender.com" 
+const myIp  = "https://leslie-gis.onrender.com"  
